7640a7f0c61214c3d91b31179a3f7c634667641d
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-06-02 18:10:09 -0300

    feat: export CurrencyField and ErrorLabel components in index file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { default as theme } from './theme';
+
+export { default as CurrencyField } from './components/CurrencyField';
+export { default as ErrorLabel } from './components/ErrorLabel';
